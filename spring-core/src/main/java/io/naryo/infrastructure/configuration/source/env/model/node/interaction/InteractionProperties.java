ac350e42e585593b543932910a726a65b8b39af4
Javier Roman <javier.rmgz@gmail.com>
2025-07-07 11:35:28 +0200

    chore: Fix paramater decoder and add default environment properties
    Signed-off-by: Javier Roman <javier.rmgz@gmail.com>

--- spring-core/src/main/java/io/naryo/infrastructure/configuration/source/env/model/node/interaction/InteractionProperties.java
+++ spring-core/src/main/java/io/naryo/infrastructure/configuration/source/env/model/node/interaction/InteractionProperties.java
@@ -1,9 +1,15 @@
 package io.naryo.infrastructure.configuration.source.env.model.node.interaction;
 
 import io.naryo.domain.node.interaction.InteractionStrategy;
+import io.naryo.infrastructure.configuration.source.env.model.node.interaction.block.BlockInteractionConfigurationProperties;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record InteractionProperties(
         @NotNull InteractionStrategy strategy,
-        @Valid @NotNull InteractionConfigurationProperties configuration) {}
+        @Valid @NotNull InteractionConfigurationProperties configuration) {
+
+    public InteractionProperties() {
+        this(InteractionStrategy.BLOCK_BASED, new BlockInteractionConfigurationProperties());
+    }
+}
