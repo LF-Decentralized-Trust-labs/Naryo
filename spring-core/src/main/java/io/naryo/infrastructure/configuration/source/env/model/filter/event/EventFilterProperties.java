f86f2e5c11552300b04ef880705fe85223b8c43f
ppolo23 <benitopolo97@gmail.com>
2025-07-21 12:21:35 +0200

    fix: set default statuses if empty in domain entity
    Signed-off-by: ppolo23 <benitopolo97@gmail.com>

--- spring-core/src/main/java/io/naryo/infrastructure/configuration/source/env/model/filter/event/EventFilterProperties.java
+++ spring-core/src/main/java/io/naryo/infrastructure/configuration/source/env/model/filter/event/EventFilterProperties.java
@@ -45,9 +45,7 @@ public abstract class EventFilterProperties extends FilterProperties
         super(id, name, FilterType.EVENT, nodeId);
         this.scope = scope;
         this.specification = specification;
-        this.statuses = statuses == null || statuses.isEmpty()
-            ? Set.of(ContractEventStatus.values())
-            : statuses;
+        this.statuses = statuses;
         this.sync = sync;
         this.visibility = visibility;
     }
