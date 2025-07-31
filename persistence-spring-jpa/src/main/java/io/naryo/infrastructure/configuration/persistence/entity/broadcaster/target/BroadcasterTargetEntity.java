6a17a28b52f8152f41a33b69360be2edcc53d3e9
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2025-07-31 09:35:19 +0200

    chore: implement broadcaster domain in persistence-spring-jpa module (#31)
    * chore: implement broadcaster domain in persistence-spring-jpa module
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    
    * chore: fix & reorganize broadcaster entity structure
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    
    ---------
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    Signed-off-by: Markel Txasko <mtxasko17@gmail.com>

---
+++ persistence-spring-jpa/src/main/java/io/naryo/infrastructure/configuration/persistence/entity/broadcaster/target/BroadcasterTargetEntity.java
@@ -0,0 +1,37 @@
+package io.naryo.infrastructure.configuration.persistence.entity.broadcaster.target;
+
+import java.util.Optional;
+
+import io.naryo.application.configuration.source.model.broadcaster.target.BroadcasterTargetDescriptor;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "broadcaster_target")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "target_type")
+@NoArgsConstructor
+@Getter
+public abstract class BroadcasterTargetEntity implements BroadcasterTargetDescriptor {
+
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
+
+    private @Nullable @NotBlank String destination;
+
+    public BroadcasterTargetEntity(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public Optional<String> getDestination() {
+        return Optional.ofNullable(this.destination);
+    }
+
+    @Override
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
