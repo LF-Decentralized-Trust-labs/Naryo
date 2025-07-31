3b0c1b90fc1df46e0e39507950775e3ef88fe6e5
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2025-07-31 09:35:19 +0200

    chore: implement broadcaster domain in persistence-spring-jpa module (#31)
    * chore: implement broadcaster domain in persistence-spring-jpa module
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    
    * chore: fix & reorganize broadcaster entity structure
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    
    ---------
    
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>
    Signed-off-by: Markel Txasko <mtxasko17@gmail.com>

---
+++ persistence-spring-jpa/src/main/java/io/naryo/infrastructure/configuration/provider/JpaBroadcasterConfigurationSourceProvider.java
@@ -0,0 +1,35 @@
+package io.naryo.infrastructure.configuration.provider;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import io.naryo.application.configuration.source.model.broadcaster.configuration.BroadcasterConfigurationDescriptor;
+import io.naryo.application.configuration.source.provider.broadcaster.BroadcasterConfigurationSourceProvider;
+import io.naryo.infrastructure.configuration.persistence.entity.broadcaster.configuration.BroadcasterConfigurationEntity;
+import io.naryo.infrastructure.configuration.persistence.repository.broadcaster.BroadcasterConfigurationEntityRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JpaBroadcasterConfigurationSourceProvider
+        implements BroadcasterConfigurationSourceProvider {
+
+    private final BroadcasterConfigurationEntityRepository repository;
+
+    public JpaBroadcasterConfigurationSourceProvider(
+            BroadcasterConfigurationEntityRepository broadcasterConfigurationRepository) {
+        this.repository = broadcasterConfigurationRepository;
+    }
+
+    @Override
+    public Collection<BroadcasterConfigurationDescriptor> load() {
+        List<BroadcasterConfigurationEntity> broadcasterConfigurationEntities =
+                this.repository.findAll();
+        return new HashSet<>(broadcasterConfigurationEntities);
+    }
+
+    @Override
+    public int priority() {
+        return 0;
+    }
+}
