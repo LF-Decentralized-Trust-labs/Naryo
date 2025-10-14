236e3893c596856201159e1a795136ca5564f09b
ppolo23 <benitopolo97@gmail.com>
2025-10-14 16:07:23 +0200

    chore: delete nodes controller
    Signed-off-by: ppolo23 <benitopolo97@gmail.com>

---
+++ configuration-spring-api/src/main/java/io/naryo/api/node/delete/DeleteNodeController.java
@@ -0,0 +1,30 @@
+package io.naryo.api.node.delete;
+
+import java.util.UUID;
+
+import io.naryo.api.node.NodeController;
+import io.naryo.application.configuration.revision.OperationId;
+import io.naryo.application.configuration.revision.operation.RemoveOperation;
+import io.naryo.application.configuration.revision.operation.RevisionOperation;
+import io.naryo.application.configuration.revision.queue.RevisionOperationQueue;
+import io.naryo.domain.node.Node;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public final class DeleteNodeController extends NodeController {
+
+    private final @Qualifier("nodeRevisionQueue") RevisionOperationQueue<Node> operationQueue;
+
+    public DeleteNodeController(RevisionOperationQueue<Node> operationQueue) {
+        this.operationQueue = operationQueue;
+    }
+
+    @PostMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public OperationId delete(@PathVariable("id") UUID id, @RequestBody DeleteNodeRequest request) {
+        RevisionOperation<Node> op = new RemoveOperation<>(id, request.getPrevItemHash());
+        return operationQueue.enqueue(op);
+    }
+}
---
+++ configuration-spring-api/src/main/java/io/naryo/api/node/delete/DeleteNodeRequest.java
@@ -0,0 +1,8 @@
+package io.naryo.api.node.delete;
+
+import lombok.Getter;
+
+@Getter
+public class DeleteNodeRequest {
+    private String prevItemHash;
+}
