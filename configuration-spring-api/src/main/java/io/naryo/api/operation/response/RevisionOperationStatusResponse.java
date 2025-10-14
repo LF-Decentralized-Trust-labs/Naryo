65767f9ecf867db53f3ac1267657ba19e027a1fa
Markel Txasko <mtxasko17@gmail.com>
2025-10-14 14:45:17 +0200

    chore: Transform RevisionOperationStatusResponse into a record
    Signed-off-by: Markel Txasko <mtxasko17@gmail.com>

--- configuration-spring-api/src/main/java/io/naryo/api/operation/response/RevisionOperationStatusResponse.java
+++ configuration-spring-api/src/main/java/io/naryo/api/operation/response/RevisionOperationStatusResponse.java
@@ -6,38 +6,16 @@ import java.util.UUID;
 import io.naryo.api.utils.IsoDateFormat;
 import io.naryo.application.configuration.revision.RevisionOperationState;
 import io.naryo.application.configuration.revision.RevisionOperationStatus;
-import lombok.Getter;
 
-@Getter
-public class RevisionOperationStatusResponse {
-    private final UUID operationId;
-    private final RevisionOperationState state;
-    private final Long revision;
-    private final String hash;
-    private final String errorCode;
-    private final String errorMessage;
-    private final @IsoDateFormat Instant acceptedAt;
-    private final @IsoDateFormat Instant lastUpdatedAt;
-
-    private RevisionOperationStatusResponse(
-            UUID operationId,
-            RevisionOperationState state,
-            Long revision,
-            String hash,
-            String errorCode,
-            String errorMessage,
-            Instant acceptedAt,
-            Instant lastUpdatedAt) {
-        this.operationId = operationId;
-        this.state = state;
-        this.revision = revision;
-        this.hash = hash;
-        this.errorCode = errorCode;
-        this.errorMessage = errorMessage;
-        this.acceptedAt = acceptedAt;
-        this.lastUpdatedAt = lastUpdatedAt;
-    }
-
+public record RevisionOperationStatusResponse(
+        UUID operationId,
+        RevisionOperationState state,
+        Long revision,
+        String hash,
+        String errorCode,
+        String errorMessage,
+        @IsoDateFormat Instant acceptedAt,
+        @IsoDateFormat Instant lastUpdatedAt) {
     public static RevisionOperationStatusResponse fromRevisionOperationStatus(
             RevisionOperationStatus revisionOperationStatus) {
         return new RevisionOperationStatusResponse(
