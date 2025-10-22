52621126505c845537e596a9be51373266e60bf0
Fran Pérez <franpersanchez@gmail.com>
2025-10-22 13:12:29 +0200

    chore: add ConfigurationApiErrors annotation to BroadcasterConfigurationController
    Signed-off-by: Fran Pérez <franpersanchez@gmail.com>

--- configuration-spring-api/src/main/java/io/naryo/api/broadcasterconfiguration/common/BroadcasterConfigurationController.java
+++ configuration-spring-api/src/main/java/io/naryo/api/broadcasterconfiguration/common/BroadcasterConfigurationController.java
@@ -1,8 +1,10 @@
 package io.naryo.api.broadcasterconfiguration.common;
 
+import io.naryo.api.error.ConfigurationApiErrors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/broadcaster-configuration")
+@ConfigurationApiErrors
 public abstract class BroadcasterConfigurationController {}
